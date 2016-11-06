0c75a0aa5a6723b2c41cf73779863e65c7fc9fb9
Steven Winston <swinston100@hotmail.com>
2016-11-06 16:15:35 +0200

    finished exercise, wk 2 day 3

---
+++ script.js
@@ -0,0 +1,55 @@
+// PROJECT EXERCISE 4 (EXTENSION)
+// Add another select to your form so that users can rate each beer from 0 to 5. 
+// Append the new information to the page.
+
+
+// PROJECT EXERCISE 5 (EXTENSION)
+// Add another button to your page that will sort the beers on the page by rating when clicked. 
+// If it gets clicked again, reverse the order (if they were sorted in ascending order, 
+// sort in descending and vice-versa).
+
+
+var beers = []
+var ascending = 1;
+
+var updateBeers = function() {
+    $('.beers-list').empty();
+    beers.forEach(function(beer) {
+        $('.beers-list').append("<li>" + beer.name + " - " + beer.user + " - " + beer.rating + "</li>")
+    })
+}
+
+var addBeer = function(beer, user, rating) {
+    beers.push({ name: beer, user: user, rating: rating })
+};
+
+$(document).ready(function() {
+    $('button').on('click', function(e) {
+        var beer = $("input[name='beerName']").val();
+        var user = $("input[name='user']").val();
+        var rating = $("select[name='rating']").val();
+        addBeer(beer, user, rating)
+        updateBeers()
+        $('.beer-form')[0].reset();
+        return false;
+    });
+
+    $('#beerSort').on('click', function(e) {
+        if (!beers.length) {
+            alert("Nothing to sort!")
+        } else {
+            beers = beers.sort(function(a, b) {
+                var nameA = a.name;
+                var nameB = b.name;
+                if (nameA < nameB) {
+                    return -ascending;
+                } else if (nameA > nameB) {
+                    return ascending;
+                } else return 0; // names must be equal
+            })
+            ascending = ascending * -1; //toggles the sort order
+            updateBeers();
+
+        }
+    });
+})
